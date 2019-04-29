c3718db7638ed891fe34e540ad23e51c36bf39f1
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2019-04-29 11:22:30 -0700

    initial setup

---
+++ src/main/java/stacksAndQueues/Queue.java
@@ -0,0 +1,13 @@
+package stacksAndQueues;
+
+public class Queue {
+
+    public Node front;
+    public Node back;
+
+    public Queue(Node front, Node back) {
+        this.front = front;
+        this.back = back;
+    }
+
+}
---
+++ src/main/java/stacksAndQueues/Stack.java
@@ -0,0 +1,10 @@
+package stacksAndQueues;
+
+public class Stack {
+    public Node top;
+
+
+    public Stack(Node top) {
+        this.top = top;
+    }
+}
