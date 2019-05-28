559fa3064a8a0da5344051b8c3e6fe4a2a233764
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2019-05-27 22:28:37 -0700

    staging for hash methods

---
+++ src/main/java/hashtable/Hashtable.java
@@ -0,0 +1,20 @@
+package hashtable;
+
+public class Hashtable<Gen> {
+
+    public void add(String key, Gen val) {
+
+    }
+
+//    public Gen get(String key) {
+//
+//    }
+
+    public boolean contains(String key) {
+        return false;
+    }
+
+//    public int hash(String key) {
+//
+//    }
+}
